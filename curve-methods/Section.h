de5f60153666a8b52e3d869e7cf7d3d566e8ee1c
dtaleme <kiwi0tails@gmail.com>
2023-06-15 15:22:18 +0300

    section

--- curve-methods/Section.h
+++ curve-methods/Section.h
@@ -1,1 +1,24 @@
 #pragma once
+#include "Curve.h"
+
+class Section : public Curve {
+ private:
+   Point a;
+   Point b;
+
+  public:
+    Section() : Curve() {};
+
+    double getX(double t) override {
+      return a.getX() + t * (b.getX() - a.getX());
+    }
+    double getY(double t) override {
+      return a.getY() + t * (b.getY() - a.getY());
+    }
+    double getXDash(double t) override {
+      return b.getX() - a.getX();
+    }
+    double getYDash(double t) override {
+      return b.getY() - a.getY();
+    }
+};
